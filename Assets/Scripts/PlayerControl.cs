0dd723b01c012f714da95989b28ab3a55451f6e6
pandaec <ericchan1026@gmail.com>
2017-04-22 23:02:22 +0800

    Revert "add player control"
    This reverts commit 5b9431273f90794a9acd3432ba9badc809c2eb2e.

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -4,38 +4,13 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour {
 
-
-    private float speed = 15f;
-    // Use this for initialization
-
-    private Rigidbody rb;
+	// Use this for initialization
 	void Start () {
         GetComponent<Renderer>().material.color = new Color(0.5f, 1, 1); //C#
-        rb = GetComponent<Rigidbody>();
     }
 	
 	// Update is called once per frame
 	void Update () {
-
-    }
-
-    void FixedUpdate() {
-        rb.velocity = new Vector2(0f, 0f);
-        if (Input.GetKey(KeyCode.W))
-        {
-            rb.velocity = new Vector2(0f, speed);
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            rb.velocity = new Vector2(0f, -speed);
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            rb.velocity = new Vector2(-speed, 0f);
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            rb.velocity = new Vector2(speed, 0f);
-        }
-    }
+		
+	}
 }
