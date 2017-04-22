6df193128ad18d2733d45470f90bbfbe47b3b95b
pandaec <ericchan1026@gmail.com>
2017-04-22 22:24:28 +0800

    add a player

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<Renderer>().material.color = new Color(0.5f, 1, 1); //C#
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
