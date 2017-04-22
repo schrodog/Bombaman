1181b56f39b8dd54f60262f1e67c754b85d70697
pandaec <ericchan1026@gmail.com>
2017-04-22 23:05:46 +0800

    add player collision

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -4,13 +4,38 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour {
 
-	// Use this for initialization
+
+    private float speed = 15f;
+    // Use this for initialization
+
+    private Rigidbody rb;
 	void Start () {
         GetComponent<Renderer>().material.color = new Color(0.5f, 1, 1); //C#
+        rb = GetComponent<Rigidbody>();
     }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+
+    }
+
+    void FixedUpdate() {
+        rb.velocity = new Vector2(0f, 0f);
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb.velocity = new Vector2(0f, speed);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            rb.velocity = new Vector2(0f, -speed);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.velocity = new Vector2(-speed, 0f);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.velocity = new Vector2(speed, 0f);
+        }
+    }
 }
